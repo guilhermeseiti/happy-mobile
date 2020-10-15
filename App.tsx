d425f0487972ce05b5cbc804fee55cef6e759d99
Guilherme Seiti <gseitihomma@gmail.com>
2020-10-15 20:32:35 -0300

    added marken on map

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MapView
-      provider={PROVIDER_GOOGLE}
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={{
           latitude: -23.5247764,
@@ -14,7 +16,15 @@ export default function App() {
           latitudeDelta: 0.008,
           longitudeDelta: 0.008
         }}
-      />
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -23.5247764,
+            longitude: -46.2023049,
+          }}
+        />
+      </MapView>
     </View>
   );
 }
