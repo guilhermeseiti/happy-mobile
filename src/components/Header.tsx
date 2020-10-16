9aae5618583acb50bcf788baa83706e8aa896431
Guilherme Seiti <gseitihomma@gmail.com>
2020-10-16 20:46:02 -0300

    implemented navigation on header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
-import {Feather} from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 interface HeaderProps {
     title: string;
+    showCancel?: boolean;
 }
 
 export default function Header(props: HeaderProps) {
+    const navigation = useNavigation();
+
+    function handleGoBackToHomePage() {
+        navigation.navigate('OrphanagesMap');
+    }
+
     return (
         <View style={styles.container}>
-            <BorderlessButton onPress={()=>{}}>
-                <Feather name="arrow-left" size={24} color="#15b6d6"/>
+            <BorderlessButton onPress={navigation.goBack}>
+                <Feather name="arrow-left" size={24} color="#15b6d6" />
             </BorderlessButton>
 
             <Text style={styles.title}>{props.title}</Text>
 
-            <BorderlessButton onPress={()=>{}}>
-                <Feather name="x" size={24} color="#ff669d"/>
-            </BorderlessButton>
+            {props.showCancel ? (<BorderlessButton onPress={handleGoBackToHomePage}>
+                <Feather name="x" size={24} color="#ff669d" />
+            </BorderlessButton>) : <View />}
         </View>
     );
 }
